74dce0d1085519ca94e78fa53b6242dad01c898b
Felipe Ramirez <90357987+framirezt@users.noreply.github.com>
2024-02-28 08:41:02 -0600

    background

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-[--color1]">
+    <main className="bg-[--color1] tex">
       <Navbar />
     </main>
   );
