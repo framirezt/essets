b2d015aab7be348b15bbd4613f1a7a1a7b7afabf
Felipe Ramirez <90357987+framirezt@users.noreply.github.com>
2024-02-28 08:11:49 -0600

    background

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="bg-[--color1]">
+      <Navbar />
+    </main>
+  );
 }
