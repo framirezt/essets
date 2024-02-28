7f65260e520dd20648f79474ca9c92bfeb550ad5
juliancon8 <jcongarcia2017@gmail.com>
2024-02-28 08:42:10 -0600

    mensaje

---
+++ components/Banner.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+import blocks from 
+
+const Banner = () => {
+  return (
+    <>
+      <div>
+        <Image className="" src={} alt="Your Company" />
+        <div>
+          <p className="text-lg">
+            Buy & Sell Real Estate <br /> With Crypto
+          </p>
+          <p className=""></p>
+          <div>
+            <button></button>
+            <button></button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
