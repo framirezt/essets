188ca7eda4f82251435b8a413b2eb25d3cd7e8fc
juliancon8 <jcongarcia2017@gmail.com>
2024-02-29 06:19:18 -0600

    Essets

--- components/InfoEssets.js
+++ components/InfoEssets.js
@@ -6,28 +6,30 @@ import exchange from "@/images/Screenshot_2024-02-24_at_5.34.15_PM-removebg-prev
 
 const InfoEssets = () => {
   return (
-    <div>
-      <div>
-        <Image src={house} alt="House" />
-        <p>
-          Now with Essets is easy to sell or purchase real estate with crypto
-          currencies
+    <div className="mt-16 flex flex-row justify-center gap-8 ">
+      <div className="rounded-custom flex flex-col items-center rounded p-4 border-2">
+        <Image className="mb-4 h-12 w-12" src={house} alt="House" />
+        <p className="text-center text-xs">
+          Now with Essets is easy to sell or <br></br>purchase real estate with
+          crypto <br /> currencies
         </p>
       </div>
-      <div>
-        <Image src={deal} alt="Deal" />
-        <p>
-          Essets is the neutral third party between buyer and seller that
-          facilitates the conversion of cryptocurrency to fiat currency in
-          partnership with recognized crypto payments provider
+      <div className="rounded-custom flex flex-col items-center rounded p-4 border-2">
+        <Image className="mb-4 h-12 w-12" src={deal} alt="Deal" />
+        <p className="text-center text-xs">
+          Essets is the neutral third party between buyer and <br /> seller that
+          facilitates the conversion of <br />
+          cryptocurrency to fiat currency in partnership with <br></br>
+          recognized crypto payments provider
         </p>
       </div>
-      <div>
-        <Image src={exchange} alt="Exchange" />
-        <p>
-          Essets can process the payment either in cryptocurrency or, in case
-          seller does not want to receive crypto,  it will convert the crypto to
-          fiat to complete the payment
+      <div className="rounded-custom flex flex-col items-center rounded-lg p-4 border-2">
+        <Image className="mb-4 h-12 w-12" src={exchange} alt="Exchange" />
+        <p className="text-center text-xs">
+          Essets can process the payment either in <br /> cryptocurrency or, in
+          case seller does not want to <br />
+          receive crypto, it will convert the crypto to fiat to <br />
+          complete the payment
         </p>
       </div>
     </div>
